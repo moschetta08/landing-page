e6b24e83477fe5d8fcd1cdd782d482ad1e7fb1c8
jeff <jmoschettabr@gmail.com>
2021-07-22 09:04:00 -0300

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,10 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
-    
     let data = {
         nome,
         email,
     }
-
     let convertData = JSON.stringify(data);
 
     localStorage.setItem('lead', convertData)
